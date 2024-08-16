1ecfddef43c3ad8e4fec88c53b665f74564ccef2
ammmar <ammaraslam7164@gmail.com>
2024-08-16 19:46:07 +0600

    authprovider  setted with jwt

--- safara-frontend/src/provider/AuthProvider.jsx
+++ safara-frontend/src/provider/AuthProvider.jsx
@@ -1,13 +1,53 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-const AuthContext = createContext();
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
+
+const provider = new GoogleAuthProvider();
+const AuthContext = createContext(null);
 const AuthProvider = () => {
+  const auth = getAuth(app);
+
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
+  const googleLogin = () => {
+    return signInWithPopup(auth, provider);
+  };
+
+  //   track user is exist
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        const userEmail = currentUser?.email;
+
+        axiosPublic.post("/jwt", userEmail).then((res) => {
+          localStorage.setItem("access-token", res.data.token);
+          setLoading(false);
+        });
+      } else {
+        localStorage.removeItem("access-token");
+        setLoading(false);
+      }
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
 
   const userInfo = {
     loading,
     user,
+    googleLogin,
   };
   return <AuthContext.Provider value={userInfo}></AuthContext.Provider>;
 };
