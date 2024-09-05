8a588bcd4570902690727e274147edae02f06d11
ammmar <ammaraslam7164@gmail.com>
2024-09-05 14:28:28 +0600

    updated ui user dashboard pages

--- safara-frontend/src/Pages/Dashboard/UserDashboard/MyClasses.jsx
+++ safara-frontend/src/Pages/Dashboard/UserDashboard/MyClasses.jsx
@@ -8,38 +8,38 @@ const MyClasses = () => {
 
   const initialIndex = categories.indexOf(category);
 
-  const [tabIndex, setTabIndex] = useState(initialIndex);
+  const [tabIndex, setTabIndex] = useState(initialIndex+1);
 
 
   return (
     <>
       <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
-          <Tab selectedClassName="bg-orange-500 text-black rounded">My Classes</Tab>
-          <Tab selectedClassName="bg-orange-500 text-black rounded">Explore</Tab>
-          <Tab selectedClassName="bg-orange-500 text-black rounded">
+          <Tab selectedClassName="bg-[#125ca6] text-white  rounded">My Classes</Tab>
+          <Tab selectedClassName="bg-[#125ca6] text-white  rounded">Explore</Tab>
+          <Tab selectedClassName="bg-[#125ca6] text-white  rounded">
         Incoming
           </Tab>
-          <Tab selectedClassName="bg-orange-500 text-black rounded">Course Details</Tab>
+          <Tab selectedClassName="bg-[#125ca6] text-white  rounded">Course Details</Tab>
         </TabList>
-        <TabPanel>unlocked courses</TabPanel>
-        <TabPanel>All Courses</TabPanel>
-        <TabPanel>Upcoming</TabPanel>
-        <TabPanel>My courses details</TabPanel>
-      </Tabs>
-
-      <div className="card md:w-96 my-10 bg-slate-300 rounded-xl shadow-xl border  border-blue-400 ">
+        <TabPanel><div className="card md:w-96 my-10 bg-slate-300 rounded-xl shadow-xl border  border-blue-400 ">
         <figure className="px-10 pt-10 ">
           <img className="rounded-xl" src={"pic"} />
         </figure>
         <div className="card-body">
           <p className="text-2xl text-[#125ca6]">
             Your running course is <br />
-            <span className="text-orange-500 text-4xl">student </span>
+            <span className=" text-4xl">student </span>
           </p>
           <div className="card-actions justify-end"></div>
         </div>
-      </div>
+      </div></TabPanel>
+        <TabPanel>All Courses</TabPanel>
+        <TabPanel>Upcoming</TabPanel>
+        <TabPanel>My courses details</TabPanel>
+      </Tabs>
+
+      
     </>
   );
 };
