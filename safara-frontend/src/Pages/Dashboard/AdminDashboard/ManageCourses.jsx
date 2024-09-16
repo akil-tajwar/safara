c162c2d447afb4289f740e0c4ea0743430cb423c
Akil Tajwar <akiltajwar@gmail.com>
2024-09-16 20:58:19 +0600

    courses data fetch in manage course page

--- safara-frontend/src/Pages/Dashboard/AdminDashboard/ManageCourses.jsx
+++ safara-frontend/src/Pages/Dashboard/AdminDashboard/ManageCourses.jsx
@@ -1,60 +1,76 @@
+import { useEffect, useState } from "react";
 import { FaEdit } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 const ManageCourses = () => {
-    return (
-        <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <th>Image</th>
-              <th>Course</th>
-              <th>Edit</th>
-              <th>Delete</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                      <img
-                        src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                        alt="Avatar Tailwind CSS Component" />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
+  const [courses, setCourses] = useState();
+  const fetchAllUsers = () => {
+    const url = `http://localhost:4000/api/course/getAllCourses`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setCourses(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Image</th>
+            <th>Course</th>
+            <th>Edit</th>
+            <th>Delete</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle h-12 w-12">
+                    <img
+                      src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                      alt="Avatar Tailwind CSS Component" />
                   </div>
                 </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-              </td>
-              <td><FaEdit className="text-3xl text-green-500"/></td>
-              <td><MdDeleteForever className="text-3xl text-red-500"/></td>
-            </tr>
-            </tbody>
-            </table>
-    
-      </div>
-    );
+                <div>
+                  <div className="font-bold">Hart Hagerty</div>
+                  <div className="text-sm opacity-50">United States</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Zemlak, Daniel and Leannon
+              <br />
+              <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+            </td>
+            <td><FaEdit className="text-3xl text-green-500" /></td>
+            <td><MdDeleteForever className="text-3xl text-red-500" /></td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  );
 };
 
 export default ManageCourses;
